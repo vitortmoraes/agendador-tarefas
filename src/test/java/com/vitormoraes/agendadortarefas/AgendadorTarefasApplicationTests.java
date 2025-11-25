11b047e3bf93a580f95a398216e5d90f28f2f472
Vitor Moraes <vitor.tm@gmail.com>
2025-11-25 20:14:13 -0300

    commit inicial com esqueleto.

---
+++ src/test/java/com/vitormoraes/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitormoraes.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
