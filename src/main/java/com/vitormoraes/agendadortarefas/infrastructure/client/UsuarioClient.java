0d06e1a37a0202401bc93e21f5ff03775a2baa52
Vitor Moraes <vitor.tm@gmail.com>
2025-11-26 11:01:05 -0300

    correção de apontamentos

--- src/main/java/com/vitormoraes/agendadortarefas/infrastructure/client/UsuarioClient.java
+++ src/main/java/com/vitormoraes/agendadortarefas/infrastructure/client/UsuarioClient.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UsuarioClient {
 
-    @GetMapping
+    @GetMapping("/usuario")
     UsuarioDTO buscaUsuarioPorEmail(@RequestParam("email") String email,
                                     @RequestHeader("Authorization") String token);
 }
